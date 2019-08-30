ed4f4410528654562f18bc7778fb7ed54ed342e0
Ben Cross <btcross26@yahoo.com>
2019-08-30 05:48:46 -0400

    Fixed math in CLogLog implementation and added small eps to prevent log(0)

--- src/genestboost/link_functions/cloglog.py
+++ src/genestboost/link_functions/cloglog.py
@@ -16,15 +16,18 @@ class CLogLogLink(BaseLink):
     """
     Complementary log-log link function
     """
+    def __init__(self, eps=1e-24):
+        super().__init__()
+        self._eps = eps
 
     def _link(self, y):
-        return np.log(-np.log(1.0 - y))
+        return np.log(-np.log(1.0 - y + self._eps))
 
     def _inverse_link(self, nu):
         return 1.0 - np.exp(-np.exp(nu))
 
     def dydnu(self, y):
-        return -(1.0 - y) * np.log(1.0 - y)
+        return -(1.0 - y) * np.log(1.0 - y + self._eps)
 
     def d2ydnu2(self, y):
-        return self.dydnu(y) * (1.0 - np.log(1.0 - y))
+        return self.dydnu(y) * (1.0 + np.log(1.0 - y + self._eps))
